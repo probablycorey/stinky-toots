b8d18e15d17e96f2a1e5f0fe81e8c04db9fc5136
Corey Johnson <probablycorey@github.com>
2018-08-20 21:13:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 //why
 app.get('/', (req, res) => {
   console.log("This is logged from the server!")
-  res.send('hey core!')
+  res.send('hey corey!')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
