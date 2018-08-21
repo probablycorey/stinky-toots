fc9cbc6b0d1b5d10edcd19bd183e6afaf327bfa5
Corey Johnson <probablycorey@github.com>
2018-08-20 21:01:33 -0700

    why

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3001
-
+//why
 app.get('/', (req, res) => {
   console.log("This is logged from the server!")
   res.send('hey core!')
