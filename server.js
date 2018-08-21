88372b110793530b99afe95ffa1f54a0603100a9
Corey Johnson <probablycorey@github.com>
2018-08-20 20:35:32 -0700

    why!

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   console.log("This is logged from the server!")
-  res.send('hey core.')
+  res.send('hey core!')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
